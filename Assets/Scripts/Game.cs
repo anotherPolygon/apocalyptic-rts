5d7aef63d816517b44f59099084ea420f8f9ae27
Shachaf <n3o@walla.co.il>
2020-05-18 17:00:02 +0300

    added a Game environment object with debug functions
    added a second camera and a canvas to serve as a debug console

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public DebugConsole DebugConsole;
+
+    // Start is called before the first frame update
+    
+}
